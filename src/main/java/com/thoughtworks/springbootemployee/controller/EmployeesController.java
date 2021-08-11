86ef5b7be28361b16369094af3405944e0ae661d
dyje <dyje@oocl.com>
2021-08-11 23:42:26 +0800

    [SpringBootEmployee][Jesse]refactor: removed unused import tags and formatted codes

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/employees")
@@ -18,38 +17,38 @@ public class EmployeesController {
     private EmployeeService employeeService;
 
     @GetMapping
-    public List<Employee> getAllEmployees(){
+    public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
     }
 
     @GetMapping(path = "/{employeeID}")
-    public Employee getEmployeeByID (@PathVariable Integer employeeID){
+    public Employee getEmployeeByID(@PathVariable Integer employeeID) {
         return employeeService.getEmployeeByID(employeeID);
     }
 
     @GetMapping(params = {"page", "pageSize"})
-    public List<Employee> getEmployeeByPage (@RequestParam Integer page, @RequestParam Integer pageSize){
-        return employeeService.getEmployeeByPage(page,pageSize);
+    public List<Employee> getEmployeeByPage(@RequestParam Integer page, @RequestParam Integer pageSize) {
+        return employeeService.getEmployeeByPage(page, pageSize);
     }
 
-    @GetMapping(params = {"gender"} )
-    public List<Employee> getEmployeeByGender (@RequestParam String gender){
+    @GetMapping(params = {"gender"})
+    public List<Employee> getEmployeeByGender(@RequestParam String gender) {
         return employeeService.getEmployeeByGender(gender);
     }
 
     @PostMapping
-    public Employee addNewEmployee (@RequestBody Employee employee){
+    public Employee addNewEmployee(@RequestBody Employee employee) {
         employeeService.addNewEmployee(employee);
         return employee;
     }
 
     @PatchMapping(path = "/{employeeId}")
-    public Employee updateEmployee (@PathVariable Integer employeeId, @RequestBody Employee employeeUpdate){
-            return employeeService.findById(employeeId, employeeUpdate);
+    public Employee updateEmployee(@PathVariable Integer employeeId, @RequestBody Employee employeeUpdate) {
+        return employeeService.findById(employeeId, employeeUpdate);
     }
 
     @DeleteMapping(path = "/{employeeId}")
-    public Employee removeEmployee (@PathVariable Integer employeeId){
+    public Employee removeEmployee(@PathVariable Integer employeeId) {
         return employeeService.removeEmployee(employeeId);
     }
 
