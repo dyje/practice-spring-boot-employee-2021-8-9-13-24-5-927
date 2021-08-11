b5f913ff130d5b7d55110a15028f8f1b5b95546d
dyje <dyje@oocl.com>
2021-08-11 21:23:44 +0800

    [SpringBootEmployee][Jesse]refactor: removed unused data

--- src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeRepository.java
@@ -11,16 +11,12 @@ public class EmployeeRepository {
     private List<Employee> employees = new ArrayList<>();
     public EmployeeRepository(){
         employees.add(new Employee(1, "Alice", 25, "Female", 10000));
-        employees.add(new Employee(2, "Bob", 25, "Female", 10000));
+        employees.add(new Employee(2, "Bob", 25, "Male", 10000));
         employees.add(new Employee(3, "Catnice", 25, "Female", 10000));
-        employees.add(new Employee(4, "Doggo", 25, "Male", 10000));
-        employees.add(new Employee(5, "Edd", 25, "Male", 10000));
-        employees.add(new Employee(6, "Farla", 25, "Female", 10000));
-        employees.add(new Employee(7, "Ginger", 25, "Female", 10000));
-        employees.add(new Employee(8, "Yondu", 25, "Male", 10000));
     }
 
-    public List<Employee> getEmployees() {
+    public List<Employee> getAllEmployees() {
         return employees;
     }
+
 }
