74d336185aedddc65c9c809297fcbf2066d7a449
dyje <dyje@oocl.com>
2021-08-12 22:52:26 +0800

    [SpringBootEmployee][Jesse] test: removed unused import tags

--- src/test/java/com/thoughtworks/springbootemployee/integration/EmployeeIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integration/EmployeeIntegrationTest.java
@@ -1,6 +1,5 @@
 package com.thoughtworks.springbootemployee.integration;
 
-import ch.qos.logback.core.encoder.EchoEncoder;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,7 +11,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.awt.*;
 import java.util.Arrays;
 import java.util.List;
 
@@ -32,15 +30,15 @@ public class EmployeeIntegrationTest {
     private List<Employee> mockEmployees;
 
     @BeforeEach
-    public void data(){
+    public void data() {
         mockEmployees = Arrays.asList(
-                (new Employee(162, "Jesse", 22,"male",500)),
-                (new Employee(195, "Donna", 26,"female",500)),
-                (new Employee(196, "Cathy", 23,"female",500)),
-                (new Employee(197, "Jerry", 25,"male",500)),
-                (new Employee(198, "Kael", 21,"male",500)),
-                (new Employee(199, "Greg", 33,"male",500)),
-                (new Employee(226, "Ramon", 21,"male",500))
+                (new Employee(162, "Jesse", 22, "male", 500)),
+                (new Employee(195, "Donna", 26, "female", 500)),
+                (new Employee(196, "Cathy", 23, "female", 500)),
+                (new Employee(197, "Jerry", 25, "male", 500)),
+                (new Employee(198, "Kael", 21, "male", 500)),
+                (new Employee(199, "Greg", 33, "male", 500)),
+                (new Employee(226, "Ramon", 21, "male", 500))
         );
     }
 
@@ -90,7 +88,7 @@ public class EmployeeIntegrationTest {
     }
 
     @Test
-    void should_return_employees_when_getEmployeeByGender() throws Exception{
+    void should_return_employees_when_getEmployeeByGender() throws Exception {
         //given
         String gender = "male";
 
@@ -125,7 +123,7 @@ public class EmployeeIntegrationTest {
     }
 
     @Test
-    void should_update_employee_info_when_updateEmployeeById() throws Exception{
+    void should_update_employee_info_when_updateEmployeeById() throws Exception {
         //given
         final Employee employee = new Employee(228, "Jennie", 22, "female", 2000);
         final Employee addedEmployee = employeeRepository.save(employee);
