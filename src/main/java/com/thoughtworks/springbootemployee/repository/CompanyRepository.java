643dcead78b83607a2e87db93001409c6bab2b26
dyje <dyje@oocl.com>
2021-08-12 17:07:53 +0800

    Revert "[SpringBootEmployee][Ramon&Jesse] feat: implementation of JPA repository for Company related files"
    This reverts commit 6dd6ab7e814da4a6ba233b07d281851203640482.

--- src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.thoughtworks.springbootemployee.repository;
-
-import com.thoughtworks.springbootemployee.model.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyRepository extends JpaRepository <Company, Integer> {
-
-}
