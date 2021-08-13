6e198d625f6098a2c18aec1b11cde1612919e9a8
dyje <dyje@oocl.com>
2021-08-13 21:08:51 +0800

    [SpringBootEmployee][Jesse] feat: implementation for exception handling for CompanyNotFoundException

---
+++ src/main/java/com/thoughtworks/springbootemployee/exception/CompanyNotFoundException.java
@@ -0,0 +1,8 @@
+package com.thoughtworks.springbootemployee.exception;
+
+public class CompanyNotFoundException extends RuntimeException{
+
+    public CompanyNotFoundException(String message){
+        super(message);
+    }
+}
