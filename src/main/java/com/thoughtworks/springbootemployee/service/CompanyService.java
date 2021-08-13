0ebb6d7a767c96a5389a4c45fe9d394295232d5a
dyje <dyje@oocl.com>
2021-08-13 18:56:45 +0800

    [SpringBootEmployee][Jesse] feat: implementation of JPA repository for getCompanyByPage()

--- src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
@@ -5,6 +5,7 @@ import com.thoughtworks.springbootemployee.exception.EmployeeNotFoundException;
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,6 +25,10 @@ public class CompanyService {
 
     public Company getCompanyById(Integer companyId){
         return companyRepository.findById(companyId).orElseThrow(()->new CompanyNotFoundException("Company ID not found."));
+    }
+
+    public List<Company> getCompanyByPage(Integer page, Integer pageSize) {
+        return companyRepository.findAll(PageRequest.of(page - 1, pageSize)).getContent();
     }
 
     public List<Employee> getAllEmployeesInCompany(Integer companyId) {
